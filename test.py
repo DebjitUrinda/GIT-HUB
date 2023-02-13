2461ef258d47fbaaadde8515b319688b1e961a92
dbjt_baki <87750461+DebjitUrinda@users.noreply.github.com>
2023-02-13 21:48:49 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,2 @@
-print("This is a test file") -> #This is written by dev from VSCode
-print("This is another line added by dev from github web") -> #This is written from GitHub Web
+print("This is a test file") #This is written by dev from VSCode
+print("This is another line added by dev from github web") #This is written from GitHub Web
